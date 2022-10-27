6dd1b45bfde2a47170889b5499e44f84fc2ea724
Caio Aranha <caranhatieri@gmail.com>
2022-10-27 13:57:00 -0300

    alo

--- Projeto 7 - DTMF/codigo_base/decode_versaoAlunos.py
+++ Projeto 7 - DTMF/codigo_base/decode_versaoAlunos.py
@@ -1,6 +1,6 @@
 
 #Importe todas as bibliotecas
-from suaBibSignal import *
+import suaBibSignal as bib
 import peakutils    #alternativas  #from detect_peaks import *   #import pickle
 import numpy as np
 import sounddevice as sd
@@ -20,13 +20,13 @@ def main():
  
     #declare um objeto da classe da sua biblioteca de apoio (cedida)   
     # algo como:
-    signal = signalMeu() 
+    signal = bib.signalMeu() 
        
     #voce importou a bilioteca sounddevice como, por exemplo, sd. entao
     # os seguintes parametros devem ser setados:
-    sd.default.samplerate = #taxa de amostragem
-    sd.default.channels = #numCanais # o numero de canais, tipicamente são 2. Placas com dois canais. Se ocorrer problemas pode tentar com 1. No caso de 2 canais, ao gravar um audio, terá duas listas
-    duration =  tempo # #tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
+    sd.default.samplerate = 44100
+    sd.default.channels = 2 #numCanais # o numero de canais, tipicamente são 2. Placas com dois canais. Se ocorrer problemas pode tentar com 1. No caso de 2 canais, ao gravar um audio, terá duas listas
+    duration =  3 # #tempo em segundos que ira aquisitar o sinal acustico captado pelo mic
     
     #calcule o numero de amostras "numAmostras" que serao feitas (numero de aquisicoes) durante a gracação. Para esse cálculo você deverá utilizar a taxa de amostragem e o tempo de gravação
 
@@ -36,20 +36,32 @@ def main():
     #Ao seguir, faca um print informando que a gravacao foi inicializada
 
     #para gravar, utilize
-    audio = sd.rec(int(numAmostras), freqDeAmostragem, channels=1)
+    audio = sd.rec(int(44100*duration), 44100, channels=1)
+    
     sd.wait()
+    print("gravacao finalizada")
+    
+    dados = audio[:,0]
+    print(dados)
+
     print("...     FIM")
+
 
 
     #analise sua variavel "audio". pode ser um vetor com 1 ou 2 colunas, lista, isso dependerá so seu sistema, drivers etc...
     #extraia a parte que interessa da gravação (as amostras) gravando em uma variável "dados". Isso porque a variável audio pode conter dois canais e outas informações). 
     
     # use a funcao linspace e crie o vetor tempo. Um instante correspondente a cada amostra!
-  
+    tempo = np.linspace(0, duration, len(dados))
     # plot do áudio gravado (dados) vs tempo! Não plote todos os pontos, pois verá apenas uma mancha (freq altas) . 
        
     ## Calcule e plote o Fourier do sinal audio. como saida tem-se a amplitude e as frequencias
-    xf, yf = signal.calcFFT(y, fs)
+    xf, yf = signal.calcFFT(dados, 44100)
+    plt.figure(1)
+    plt.plot(xf, yf)
+    #plt.axis([0, 2000, 0, 3*44100])
+    plt.title("Fourier")
+    plt.show()
     
     #agora, voce tem os picos da transformada, que te informam quais sao as frequencias mais presentes no sinal. Alguns dos picos devem ser correspondentes às frequencias do DTMF!
     #Para descobrir a tecla pressionada, voce deve extrair os picos e compara-los à tabela DTMF
@@ -60,8 +72,16 @@ def main():
     # "thres" determina a sensibilidade da funcao, ou seja, quao elevado tem que ser o valor do pico para de fato ser considerado um pico
     #"min_dist" é relatico tolerancia. Ele determina quao próximos 2 picos identificados podem estar, ou seja, se a funcao indentificar um pico na posicao 200, por exemplo, só identificara outro a partir do 200+min_dis. Isso evita que varios picos sejam identificados em torno do 200, uma vez que todos sejam provavelmente resultado de pequenas variações de uma unica frequencia a ser identificada.   
     # Comece com os valores:
-    index = peakutils.indexes(yf, thres=0.4, min_dist=50)
+    index = peakutils.indexes(yf, thres=0.05, min_dist=50)
     print("index de picos {}" .format(index)) #yf é o resultado da transformada de fourier
+    frequencias = xf[index]
+    print("frequencias {}" .format(frequencias))
+
+    #linha
+    valor = 0
+
+
+   
 
     #printe os picos encontrados! 
     # Aqui você deverá tomar o seguinte cuidado: A funcao  peakutils.indexes retorna as POSICOES dos picos. Não os valores das frequências onde ocorrem! Pense a respeito
