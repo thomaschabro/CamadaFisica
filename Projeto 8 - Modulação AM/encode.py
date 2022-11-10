2c9ac1ebf877fdea0bd18ceb4dee7b61c3807788
Caio Aranha <caranhatieri@gmail.com>
2022-11-10 14:33:35 -0300

    comis

--- Projeto 8 - Modulação AM/encode.py
+++ Projeto 8 - Modulação AM/encode.py
@@ -6,6 +6,9 @@ import sounddevice as sd
 import matplotlib.pyplot as plt
 import math
 import time
+import soundfile as sf
+import filtro as f1
+import filtro2 as f2
 
 def main():
     signal = bib.signalMeu() 
@@ -14,13 +17,28 @@ def main():
     duration = 5  # seconds
     
     # ----------- Gravando o sinal -----------
-    audio = sd.rec(int(44100*duration), 44100, channels=1)
-
-    sd.wait()
-    print("gravacao finalizada")
+    # audio = sd.rec(int(44100*duration), 44100, channels=1)
     
-    dados = audio[:,0]
+    # sd.wait()
+
+    # sf.write('audio.wav', audio, 44100)
+    
+
+  
+    print("gravacao finalizada")
+    #salva audio
+    
+    audio = sf.read('audio.wav',44100,5)
+
+
+    dados = audio[0]
     print(dados)
+
+    filtrado = f1.filtro(dados, 44100, 2200)
+    sd.play(dados, 44100)    
+    sd.wait()
+
+
 
     print("...     FIM")
     # ----------- Fim da gravação -----------
